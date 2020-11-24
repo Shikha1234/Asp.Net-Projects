41dbd09ee25d752cb91d9a70000c287589a8f455
Shikha1234 <shikhapandey95@gmail.com>
2020-11-24 15:30:07 +0530

    Implementation of call by Reference. Ref and Out Keyword

---
+++ CallByReferenceDemo/MethodsDemo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallByReferenceDemo
+{
+    class Institute 
+    {
+        private string _CourseName;
+        private decimal _Fees;
+
+        public void SetCourse(string cname , decimal fees) 
+        {
+            this._CourseName = cname;
+            this._Fees = fees;
+        }
+
+        public string GetCourseName() 
+        {
+            return this._CourseName;
+        }
+        public decimal GetFees()
+        {
+            return this._Fees;
+        }
+
+        //Here we are returning discount argment hende it has bee assigned as our argument
+        // Difference between out and return 
+        //Return only returns a single value 
+        //but out can return multiple values by just defining multiple argument in the method 
+        //with "out"keyword
+
+        public void SetDiscount(decimal percentage, out decimal discount ) 
+        {
+            discount = percentage * 10;
+        }
+
+        // Here we are passing fees as call by reference so once this fees variable updated it will update the caller vale
+        //Fees = Call by Reference Argument
+
+
+        public void UpdateCourseFess(ref decimal fees , decimal discount) 
+        {
+            fees -= discount;
+        }
+    }
+
+    class MethodsDemo
+    {
+        static void Main(string[] args) 
+        {
+            Institute ins = new Institute();
+            ins.SetCourse(".Net", 200000);
+            string cname = ins.GetCourseName();
+            decimal fees = ins.GetFees();
+            Console.WriteLine("Course name : {0} \n Fees: Rs.{1}", cname, fees);
+            decimal percentage;
+            Console.WriteLine("ENter your Percentage : ");
+            percentage = decimal.Parse(Console.ReadLine());
+            decimal discount;
+            ins.SetDiscount(percentage, out discount);
+            Console.WriteLine("Congrats !! you have receive {0} rupees of discount", discount);
+            ins.UpdateCourseFess(ref fees, discount);
+            Console.WriteLine("Final course Fees is : {0} ", fees);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallByReferenceDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CallByReferenceDemo
+{
+    class CBRClass 
+    { 
+        public void Swap(ref int no1 , ref int no2) 
+        {
+            // When we are calling value by reference if any changes has made in the varible it beenre
+            //reflected to called also
+            int temp = no1;
+            no1 = no2;
+            no2 = temp;
+        }
+     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no1, no2;
+            Console.WriteLine("Enter two numbers");
+            no1 = int.Parse(Console.ReadLine());
+            no2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before swapping values are :{0} {1} ",no1,no2);
+            CBRClass cb = new CBRClass();
+            cb.Swap(ref no1, ref no2);
+            Console.WriteLine("After swap method called {0} {1}",no1,no2);
+            Console.ReadLine();
+        }
+    }
+}
+ 
