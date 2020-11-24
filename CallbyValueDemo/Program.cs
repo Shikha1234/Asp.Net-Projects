e3436910a4507814cc0f63221b95305d74fc39da
Shikha1234 <shikhapandey95@gmail.com>
2020-11-24 14:50:35 +0530

    Created Call by Value , Named Parameters and Optional Parameters.

---
+++ CallbyValueDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbyValueDemo
+{
+    class Name
+    {
+        private string _firstName, _lastName;
+        public void SetName(string fname , string lname) 
+        {
+            this._firstName = fname;
+            this._lastName = lname;
+        }
+        public string GetName() 
+        {
+            return String.Format("{0} {1}", _firstName, _lastName);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Name name = new Name();
+           
+            //Call by value 
+            //name.SetName("shikha", "pandey");
+
+            //Named paramter passing
+            name.SetName(fname: "Shikha", lname: "Pandey");
+
+            Console.WriteLine("Name :{0}", name.GetName());
+            Console.ReadLine();
+
+        }
+    }
+}
