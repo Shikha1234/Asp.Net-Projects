cf72f55ccac04f42b71b1963477c69f48ec6ee0d
Shikha1234 <shikhapandey95@gmail.com>
2020-11-24 18:01:50 +0530

    created project for interface and abstract classes demo.

---
+++ InterfaceDemo/OOPsDemo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+    class Institute 
+    {
+        public virtual void CourseOffered() 
+        {
+            Console.WriteLine("Course Offered C, C++ , .Net ,JAVA");
+        }
+
+        public void Discount() 
+        {
+            Console.WriteLine("Default discount : Rs 500/-");
+        }
+    }
+
+
+    interface IBanners 
+    {
+        void Location();
+    
+    }
+
+    interface IAds 
+    {
+        void PaperDetails();
+    }
+
+
+    class ShikhaTechnos : Institute, IBanners, IAds
+    {
+        public override void CourseOffered()
+        {
+            Console.WriteLine("Course Offered Python , Java ,net , Scirpt ");
+        }
+
+        public void Location()
+        {
+            Console.WriteLine("Banners Pune Maharashtra");
+
+        }
+
+        public void PaperDetails()
+        {
+            Console.WriteLine("Papers Ads @ TOI, Danik Bhaskar");
+        }
+    }
+
+
+    class Demo 
+    {
+        static void main(string[] args) 
+        {
+            ShikhaTechnos sp = new ShikhaTechnos();
+            sp.CourseOffered();
+            Institute ins = new Institute();
+            ins.Discount();
+            sp.Discount();
+            sp.Location();
+            Console.WriteLine("---------------------------------------------");
+
+            
+
+        
+        }
+    
+    }
+}
---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    interface IFather 
+    {
+        void Loan();
+    }
+
+    interface IMother
+    {
+        void Loan();
+    }
+
+    class Child2 : IFather, IMother
+    {
+        void IFather.Loan() 
+        {
+            Console.WriteLine("Child2 fathers 20% loan will be clear...");
+        }
+
+        void IMother.Loan()
+        {
+            Console.WriteLine("Child2 Mothers 5 0% loan will be clear...");
+        }
+    }
+
+    class Child1 : IMother, IFather
+    {
+        public void Loan()
+        {
+            Console.WriteLine("Child1 remaining father and mother loan will be cleared...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child1 c1 = new Child1();
+            Child2 c2 = new Child2();
+
+            //When ever we have defined interface explicilty we need to create variable to use that interface.
+
+            IFather father = c2 as IFather;
+            IMother mother = c2 as IMother;
+
+            father.Loan();
+            mother.Loan();
+
+            Console.WriteLine("------------------------------------");
+
+            //Wheneve we use public method  to provide thre implementations for the interface method 
+            // then we can directly invoke using  objec tnam e
+
+            c1.Loan();
+            Console.ReadLine();
+        }
+    }
+}
